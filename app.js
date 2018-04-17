9da27b6f0692a30be83451df3d0e5d98acc4d724
Stefan Sathianathen <stefansathianathen@Stefans-MacBook-Pro.local>
2018-04-17 22:53:12 +1000

    Linked static site

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express = require('express');
-
+var path = require("path");
 var app = express();
-var router = require('./route/router');
+//var router = require('./route/router');
 
-app.set('view engine', 'ejs');
-app.use( express.static( "public" ) );
+//app.set('view engine', 'html');
+//app.use( express.static( "public" ) );
+app.use(express.static(path.join(__dirname+'/mypages')));
 
-app.use('/', router);
+
+app.get('/*', function (req, res) {
+    res.sendFile('landing.html', {root: path.join(__dirname+'/mypages')});
+});
+
+//app.use('/', router);
 const PORT = process.env.PORT|| 3000;
 
 app.listen(PORT, function(){
 	console.log(`Express listening on port ${PORT}`);
 	}
 	);
+
