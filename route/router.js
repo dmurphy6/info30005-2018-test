b2033e2fd4631ebdaa5494b8895678633a792c74
Jeremy <jeremytee97@gmail.com>
2018-05-21 13:58:21 +1000

    lots of changes, creation of users, creation of memorials, added alert messages

--- route/router.js
+++ route/router.js
@@ -1,118 +1,263 @@
-var express = require('express');
-var User = require('../models/user');
-var userController = require('../controllers/userController');
-var router = express.Router();
+// var users = require('../models/db');
 var visits = 0;
+var userController = require('../controllers/userControllers');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var fs = require('fs');
+var ObjectId = require('mongodb').ObjectID;
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
+
+module.exports = function (router, passport) {
+    router.get('/', function (req, res) {
+        res.render('landing', {message: req.flash('landingInfoMessage')});
+        visits += 1;
+        console.log(visits);
+    });
+
+    router.get('/landing', function (req, res) {
+        res.render('landing', {message: req.flash('landingInfoMessage')});
+    });
+
+    router.get('/writeLegacy', userController.getWriteLegacy);
+
+    router.get('/readLegacy', userController.getReadLegacy);
+
+    router.get('/aboutus', function (req, res) {
+        res.render('aboutus.ejs');
+    });
+
+    router.get('/aboutusAccessFromLanding', function (req, res) {
+        res.render('aboutusAccessFromLanding.ejs');
+    });
+
+    router.get('/blog', userController.getBlog);
+
+    router.get('/blogAccessFromLanding', userController.getBlogFromLanding);
+
+    router.get('/changePassword', userController.getChangePassword);
+
+    router.get('/imagegallery', function (req, res) {
+        res.render('imagegallery.ejs');
+    });
+
+    router.get('/inviteContributer', function (req, res) {
+        res.render('inviteContributer.ejs');
+    });
+
+    router.get('/login', function (req, res) {
+        res.render('login.ejs', {message: req.flash('loginMessage')});
+    });
+    router.get('/viewProfile', userController.getViewProfile);
+
+    router.get('/profile', isLoggedIn, userController.getProfilePage);
+
+    /*router.get('../ownProfile/:id',isLoggedIn, userController.getProfilePage);
+*/
+    router.get('/signup', userController.getSignUp);
+
+    router.get('/firstInformation', userController.getFirstInformation);
+
+    router.get('/nextInformation', userController.getNextInformation);
+
+    router.get('/finalInformation', userController.getFinalInformation);
+
+    router.get('/firstInformationLogin', userController.getFirstInformationLogin);
+
+    router.get('/nextInformationLogin', userController.getNextInformationLogin);
+
+    router.get('/finalInformationLogin', userController.getFinalInformationLogin);
+
+    router.get('/settings', userController.getSettings);
+
+    router.get('/createMemorial', userController.getCreateMemorial);
+
+    router.post('/login', passport.authenticate('local-login', {
+            failureRedirect: '/login',
+            failureFlash: true
+        }),
+        userController.getCreateMemorial);
+
+
+    router.post('/signup', passport.authenticate('local-signup', {
+            failureRedirect: '/signup',
+            failureFlash: true
+        }),
+        userController.getCreateMemorial);
+
+    router.post('/firstInfoLogin', userController.firstInformationLogin);
+
+    router.post('/secondInfoLogin', userController.secondInformationLogin);
+
+    router.post('/finalInfoLogin', userController.finalInformationLogin);
+
+    router.post('/firstInfo', userController.firstInformation);
+
+    router.post('/secondInfo', userController.secondInformation);
+
+    router.post('/finalInfo', userController.finalInformation);
+
+    router.post('/changePassword', userController.changePassword);
+
+    router.post('/saveLegacy', userController.saveLegacy);
+
+    router.post('/upload', upload.single('upl'), function (req, res, next) {
+        // req.file is the `avatar` file
+        // req.body will hold the text fields, if there were any
+        console.log(req.file.data);
+        if (req.session && req.user) {
+            User.findOne({ username: req.user.username }, function(err, user) {
+                if (user) {
+                    req.user = user;
+                }
+                // finishing processing the middleware and run the route
+                user.profileImg.data = fs.readFileSync(req.file.path);
+                user.profileImg.contentType = req.file.mimetype;
+
+                user.save(function(err) {
+                    if (err) throw err;
+                    res.contentType(user.profileImg.contentType);
+                    res.send(user.profileImg.data);
+                });
+            })
+        } else {
+            req.flash('profileInfoMessage', 'Error has occurred.');
+            res.redirect('profile');
+        }
+    });
+};
+
+
+// router.get('/tqCard', function (req, res) {
+// 	res.render('tqCard.ejs');
+// })
+//
+
+
+
+
+// load all the things we need
+var LocalStrategy   = require('passport-local').Strategy;
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
-passport.use('local',new LocalStrategy({
-    passReqToCallback : true
-  },
-  function(req,username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-      	console.log('bad username');
-        return done(null, false, req.flash('message', 'User Not found.'));
-      }
-      if (!user.validPassword(password)) {
-      	console.log('bad pas');
-        return done(null, false, req.flash('message', 'Invalid Password'));
-      }
-      return done(null, user);
+// load up the user model
+var models = require('../models/user');
+
+    // =========================================================================
+    // passport session setup ==================================================
+    // =========================================================================
+    // required for persistent login sessions
+    // passport needs ability to serialize and unserialize users out of session
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
     });
-  }
-));
 
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  // console.log(`id: ${id}`);
-  User.findById(id)
-    .then((user) => {
-      done(null, user);
-    })
-    .catch((error) => {
-      console.log(`Error: ${error}`);
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        models.User.findById(id, function(err, user) {
+            done(err, user);
+        });
     });
-});
 
+    // =========================================================================
+    // LOCAL SIGNUP ============================================================
+    // =========================================================================
+    // we are using named strategies since we have one for login and one for signup
+    // by default, if there was no name, it would just be called 'local'
 
-router.get('/', function (req, res) {
-	res.render('landing.ejs');
-	visits += 1;
-	console.log(visits);
-})
+    passport.use('local-signup', new LocalStrategy({
+            // by default, local strategy uses username and password, we will override with email
+            usernameField : 'Username',
+            passwordField : 'Password',
+            emailField: 'email',
+            passReqToCallback : true // allows us to pass back the entire request to the callback
+        },
+        function(req, username, password, done) {
+            //
+            // console.log(username);
+            // console.log(req.body.email);
+            // console.log(password);
 
-router.get('/landing',function (req, res) {
-	req.logout();
-	res.render('landing');
-})
+            // asynchronous
+            // models.User.findOne wont fire unless data is sent back
+            process.nextTick(function() {
 
+                // find a user whose email is the same as the forms email
+                // we are checking to see if the user trying to login already exists
+                models.User.findOne({ 'username' :  username }, function(err, user) {
+                    // if there are any errors, return the error
+                    if (err) {
+                        return done(err);
+                    }
 
-router.get('/aboutus', function (req, res) {
-	res.render('aboutus.ejs');
-})
+                    // check to see if theres already a user with that username
+                    if (user) {
+                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+                    } else {
+                        //check if password matches
+                        if(req.body.Password != req.body.confirmPassword){
+                            return done(null, false, req.flash('signupMessage', 'Password does not match.')); // create the loginMe
+                        }
 
-router.get('/blog', userController.blog);
+                        // if there is no user with that username
+                        // create the user
+                        var newUser      = new models.User();
 
-router.get('/blogAccessFromLanding', userController.blog);
+                        // set the user's local credentials
+                        newUser._id = ObjectId();
+                        newUser.username = username;
+                        newUser.password = newUser.generateHash(password);
+                        newUser.email = req.body.email;
 
-router.get('/imagegallery', userController.getImages);
+                        // save the user
+                        newUser.save(function(err) {
+                            if (!err) {
+                                req.session.user = newUser;
+                            }
+                            else {
+                                throw err;
+                            }
+                            console.log('req.session: '+ req.session.user.username);
+                            return done(null, newUser);
+                        });
+                    }
 
+                });
 
-router.get('/inviteContributer', function (req, res) {
-	res.render('inviteContributer.ejs');
-})
+            });
 
-router.get('/login', function (req, res) {
-	res.render('login.ejs', { message: req.flash('message') });
-})
+        }));
 
-router.get('/profilePage', userController.getUser);
+    passport.use('local-login', new LocalStrategy({
+            // by default, local strategy uses username and password, we will override with email
+            usernameField : 'username',
+            passwordField : 'password',
+            passReqToCallback : true // allows us to pass back the entire request to the callback
+        },
+        function(req, username, password, done) { // callback with email and password from our form
 
-router.get('/user/:user',userController.getOtherUser);
-router.get('/imagegallery/:user',userController.getOtherUserImages);
+            // find a user whose email is the same as the forms email
+            // we are checking to see if the user trying to login already exists
+            models.User.findOne({ 'username' :  username }, function(err, user) {
+                // if there are any errors, return the error before anything else
+                if (err)
+                    return done(err);
 
-router.get('/signup', function (req, res) {
-	res.render('signup.ejs');
-})
+                // if no user is found, return the message
+                if (!user)
+                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                // if the user is found but the password is wrong
+                if (!user.validPassword(password))
+                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                // all is well, return successful user
 
-router.get('/tqCard', function (req, res) {
-	res.render('tqCard.ejs');
-})
-
-router.get('/writeLegacy', function (req, res) {
-	res.render('writeLegacy.ejs');
-})
-
-router.get('/aboutUser', function (req, res) {
-	res.render('getInfo.ejs');
-})
-
-router.get('/info', function (req, res) {
-	res.render('information.ejs');
-})
-
-router.get('/lastInfo', function (req, res) {
-	res.render('finalInfo.ejs');
-})
-
-router.post('/createUser', userController.createUser);
-router.post('/profilePage', 
-	passport.authenticate('local', { successRedirect: '/profilePage',
-                                  	failureRedirect : '/login',
-    							   failureFlash: true ,
-                                   session: true }));
-
-router.post("/nextInfo", userController.aboutUser);
-router.post('/final', userController.finalInfo);
-router.post('/alldone', userController.finaladditions);
-router.post('/addImage', userController.addImage);
-router.post('/user/:user', userController.getOtherUser);
-router.post('/addcontributer', userController.addcontributer);
-module.exports = router;
+                return done(null, user);
+            });
+        }));
