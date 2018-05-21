bda03b6425f3a6fc69c76eff491d8974d510c364
Jeremy <jeremytee97@gmail.com>
2018-05-21 14:04:50 +1000

    added new router, install bcrypt, install morgan, install mutler, installed fs

--- app.js
+++ app.js
@@ -1,64 +1,43 @@
 var express = require('express');
-
 var app = express();
-var router = require('./route/router');
 var bodyParser = require('body-parser');
-var flash = require('connect-flash');
-var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var flash = require('connect-flash');
+var session = require('express-session');
+var morgan = require('morgan');
+var router = express.Router();
+
+
+require('./route/router');
 require('./models/db');
-var cookieParser = require('cookie-parser');
-app.use(cookieParser());
+
+// required for passport
+app.use(session({ resave: true ,secret: '123456' , saveUninitialized: true})); // session secret
 
 
+//app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+// app.use(session({ secret: 'shhsecret' }));
 
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
-app.use(express.static( "public" ));
-
-app.use(session({
-    secret: 'abcdefg',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(flash());
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+app.set('view engine', 'ejs');
+app.use( express.static( "public" ) );
+app.use(router);
 
 
+// routes ======================================================================
+require('./route/router')(router, passport); // load our routes and pass in our app and fully configured passport
 
+const PORT = process.env.PORT|| 3000;
 
-app.use('/', router);
-app.use('/landing', router);
-app.use('/aboutus',router);
-app.use('/blog',router);
-app.use('/blogAccessFromLanding',router);
-app.use('/imagegallery',router);
-app.use('/inviteContributer',router);
-app.use('/login',router);
-app.use('/profilePage',router);
-app.use('/user/:user',router);
-app.use('/signup',router);
-app.use('/tqCard',router);
-app.use('/writeLegacy',router);
-app.use('/createUser',router);
-app.use('/aboutUser',router);
-app.use('/nextInfo',router);
-app.use('/info',router);
-app.use('/final',router);
-app.use('/lastInfo',router);
-app.use('/alldone',router);
-app.use('/addImage',router);
-app.use('/addcontributer',router);
-
-var port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-    console.log("App is running on port " + port);
-});
-
-
+app.listen(PORT, function(){
+	console.log(`Express listening on port ${PORT}`);
+	}
+);
